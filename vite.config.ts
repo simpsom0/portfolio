0466e23ca276038bdef856e2ca7e1df0873893f6
Michael <simmichaeld@gmail.com>
2025-10-16 08:48:50 -0400

    added unit testing and pre-commit hook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    ...configDefaults,
+    environment: 'jsdom',
+    globals: true,
+  },
 });
