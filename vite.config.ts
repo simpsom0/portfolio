cd4b52b9d3e323ec8ef010f547a553d8ac29273c
Michael <simmichaeld@gmail.com>
2025-10-27 08:19:29 -0400

    added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
   test: {
     ...configDefaults,
     environment: 'jsdom',
