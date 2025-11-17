899c2e5a65f82c5e5b2219f978bdec6850d44f60
Michael <simmichaeld@gmail.com>
2025-11-16 19:05:33 -0500

    removed placeholder description

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -4,15 +4,6 @@ function Body() {
   return (
     <div className="body-container">
       <h1 className="title">Michael Simpson</h1>
-      <div className="description">
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus
-        ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
-        tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
-        Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis
-        massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper
-        vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-        inceptos himenaeos.
-      </div>
     </div>
   );
 }
