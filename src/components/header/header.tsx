dc275ef8e635134c5e9c44219bb3300c83eac01b
Michael <simmichaeld@gmail.com>
2026-01-27 10:23:46 -0500

    added theme variables and toggle button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,17 @@
 import './header.scss';
 import Github from '@components/shared/links/github/github';
 import Linkedin from '@components/shared/links/linkedin/linkedin';
+import ThemeButton from '@components/shared/theme/theme-button/theme-button';
 
 function Header() {
   return (
-    <div className="links">
-      <Github />
-      <Linkedin />
+    <div className="header">
+      <ThemeButton />
+      <div className="divider"></div>
+      <div className="links">
+        <Github />
+        <Linkedin />
+      </div>
     </div>
   );
 }
---
+++ src/components/shared/theme/theme-button/theme-button.tsx
@@ -0,0 +1,60 @@
+import { useTheme } from '../use-theme';
+import './theme-button.scss';
+
+const Sun = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="icon"
+    >
+      <circle cx="12" cy="12" r="4" />
+      <path d="M12 2v2" />
+      <path d="M12 20v2" />
+      <path d="m4.93 4.93 1.41 1.41" />
+      <path d="m17.66 17.66 1.41 1.41" />
+      <path d="M2 12h2" />
+      <path d="M20 12h2" />
+      <path d="m6.34 17.66-1.41 1.41" />
+      <path d="m19.07 4.93-1.41 1.41" />
+    </svg>
+  );
+};
+
+const Moon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="icon"
+    >
+      <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+    </svg>
+  );
+};
+
+function ThemeButton() {
+  const { theme, setTheme } = useTheme();
+
+  const invertTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+  };
+
+  const icon = theme === 'dark' ? <Moon /> : <Sun />;
+
+  return <button onClick={invertTheme}>{icon}</button>;
+}
+
+export default ThemeButton;
