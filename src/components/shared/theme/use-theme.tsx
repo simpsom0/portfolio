208987f3c4d995afb1a3d3c401082c6504146f88
Michael <simmichaeld@gmail.com>
2026-01-27 10:21:28 -0500

    added theme context and useTheme api

---
+++ src/components/shared/theme/use-theme.tsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from 'react';
+import type { ThemeProviderState } from './theme-provider';
+
+const initialState: ThemeProviderState = {
+  theme: 'system',
+  setTheme: () => null,
+};
+
+export const ThemeProviderContext = createContext(initialState);
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined)
+    throw new Error('useTheme must be used within a ThemeProvider');
+
+  return context;
+};
