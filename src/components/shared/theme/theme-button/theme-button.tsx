05b2807de2cc167a75959301b660c38c830cf3b5
Michael <simmichaeld@gmail.com>
2026-02-02 08:45:57 -0500

    fixed svg attributes

--- src/components/shared/theme/theme-button/theme-button.tsx
+++ src/components/shared/theme/theme-button/theme-button.tsx
@@ -9,9 +9,9 @@ const Sun = () => {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon"
     >
       <circle cx="12" cy="12" r="4" />
@@ -35,9 +35,9 @@ const Moon = () => {
       height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon"
     >
       <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
