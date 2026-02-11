f132c2408e0b7f8c767214d0060f8f9b8400f7a4
Michael <simmichaeld@gmail.com>
2026-02-11 08:47:49 -0500

    moved icon types and svgs to types folder

--- src/hooks/icon-activity/icon-activity-provider.tsx
+++ src/hooks/icon-activity/icon-activity-provider.tsx
@@ -1,28 +1,6 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import { IconActivityContext } from './use-icon-activity';
-
-export const IconKey = {
-  Angular: 0,
-  Vue: 1,
-  Sass: 2,
-  Tailwind: 3,
-  Python: 4,
-  Go: 5,
-  Sql: 6,
-  Postgres: 7,
-  Sqlite: 8,
-  React: 9,
-  Typescript: 10,
-  Html: 11,
-  Css: 12,
-  CSharp: 13,
-  Git: 14,
-  Azure: 15,
-  Aws: 16,
-  Databricks: 17,
-  Docker: 18,
-} as const;
-export type IconKeyType = (typeof IconKey)[keyof typeof IconKey];
+import type { IconKeyType } from '@/types/icons/iconKey.d';
 
 export type IconActivityState = {
   iconActivity: Set<IconKeyType>;
